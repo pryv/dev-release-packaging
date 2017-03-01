fe692a43d90af688e38d3f0767f9337a749c9950
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2017-03-01 13:27:10 +0100

    Also adds 'yarn' binary to base image

--- build/base/configure.sh
+++ build/base/configure.sh
@@ -5,5 +5,12 @@ source /pd_build/buildconfig
 # Disable CRON for now. 
 touch /etc/service/cron/down
 
+# Remove services from our own base image that we will not use now: 
+rm -r /etc/service/syslog-ng
+rm -r /etc/service/syslog-forwarder
+
 # Create base directory for Pryv installs
 mkdir -p /var/pryv
+
+# Install yarn
+npm install -g yarn
--- build/base/prepare.sh
+++ build/base/prepare.sh
@@ -11,7 +11,3 @@ run usermod -L app
 run mkdir -p /home/app/.ssh
 run chmod 700 /home/app/.ssh
 run chown app:app /home/app/.ssh
-
-# Remove services from our own base image that we will not use now: 
-rm -r /etc/service/syslog-ng
-rm -r /etc/service/syslog-forwarder
