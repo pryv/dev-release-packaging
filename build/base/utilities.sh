80a8ad37cdc43594040b39eeaf1f1dfb827e4acd
Kaspar Schiess <kaspar@pryv.com>
2017-05-10 16:03:15 +0200

    Ok, should've caught that

--- build/base/utilities.sh
+++ build/base/utilities.sh
@@ -8,4 +8,4 @@ run minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
 run minimal_apt_get_install git
 ## We need this for dist directory prep
-run minimal_apt_get_install dist
+run minimal_apt_get_install rsync
