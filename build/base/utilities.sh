363ab1fcf4b950afe19678f050270040595caaf0
Kaspar Schiess <kaspar@pryv.com>
2017-05-10 15:57:10 +0200

    Add rsync to base image

--- build/base/utilities.sh
+++ build/base/utilities.sh
@@ -7,4 +7,5 @@ source /pd_build/buildconfig
 run minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
 run minimal_apt_get_install git
-
+## We need this for dist directory prep
+run minimal_apt_get_install dist
