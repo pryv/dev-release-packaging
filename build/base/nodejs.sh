77bc0550d837399f322e406ce9e428c4aae074e1
Kaspar Schiess <kaspar@pryv.com>
2017-10-27 15:02:52 +0200

    Update node version to 8.8.1
    Versions 4.8.2 and later, 6.10.2 and later, as well as all versions of 8.x are
    vulnerable to an issue that can be used by an external attacker to cause a
    denial of service. The severity of this vulnerability is HIGH and users of the
    affected version should plan to upgrade when a fix is made available.
    
    https://archive.is/LRy8f

--- build/base/nodejs.sh
+++ build/base/nodejs.sh
@@ -5,12 +5,12 @@ source /pd_build/buildconfig
 header "NodeJs installation."
 
 ## Install Node.js
-nodeversion=v8.2.1
+nodeversion=v8.8.1
 version=x64
 targz="node-${nodeversion}-linux-${version}.tar.gz"
 binary_url="http://nodejs.org/dist/${nodeversion}/${targz}"
 local_file="/var/spool/${targz}"
-shasum="c082cf6e7011d4222d476c86421ae6a656d7d34d3d34133260e19a7718de88c9"
+shasum="df83beb05af3e7aee4d16b74dd6d05967f47ee4ab6d6789ca0ed7f2b22c22c92"
 
 cd /var/spool
 
