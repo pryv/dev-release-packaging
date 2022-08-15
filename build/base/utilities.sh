2f42a5a7716cb06b523512281755c8542cbe7753
Simon Goumaz <simon@attentif.ch>
2022-08-15 13:55:50 +0200

    Cleanup obsolete stuff

--- build/base/utilities.sh
+++ build/base/utilities.sh
@@ -3,9 +3,9 @@
 set -e
 source /pd_build/buildconfig
 
-## Many Ruby gems and NPM packages contain native extensions and require a compiler.
+## Many NPM packages contain native extensions and require a compiler
 run minimal_apt_get_install build-essential
-## Bundler has to be able to pull dependencies from git.
+## Bundler has to be able to pull dependencies from git
 run minimal_apt_get_install git
 ## We need this for dist directory prep
 run minimal_apt_get_install rsync
