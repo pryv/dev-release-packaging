4e8ea1683dd0f8e8659a9adc9454eb75739277da
Simon Goumaz <simon@attentif.ch>
2022-12-19 14:37:42 +0100

    Refactor, step 1/2

--- docker/mongodb/finalize.sh
+++ docker/mongodb/finalize.sh
@@ -8,4 +8,3 @@ run apt-get remove -y autoconf automake
 run apt-get autoremove
 run apt-get clean
 run rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-run rm -rf /pd_build
--- docker/nginx/finalize.sh
+++ docker/nginx/finalize.sh
@@ -4,8 +4,6 @@ source /pd_build/buildconfig
 
 header "Finalizing"
 
-run apt-get remove -y autoconf automake
 run apt-get autoremove
 run apt-get clean
 run rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-run rm -rf /pd_build
