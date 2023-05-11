acaba7f4249153c49f1dc625e157817a7d3dd782
Perki <perki@pryv.com>
2023-05-11 15:10:04 +0200

    using mongo 6.0

--- docker/mongodb/install.sh
+++ docker/mongodb/install.sh
@@ -9,13 +9,20 @@ main() {
 
 mongodb() {
   # Versions and urls/paths
-  mongo_tar_gz="mongodb-linux-x86_64-3.6.23.tgz"
-  mongo_url="https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.6.23.tgz"
+  mongo_tar_gz="mongodb-linux-x86_64-ubuntu2204-v6.0-latest.tgz"
+  mongo_url="https://fastdl.mongodb.org/linux/"${mongo_tar_gz}
+
+  mongotools_tar_gz="mongodb-database-tools-ubuntu2204-x86_64-100.7.0.tgz"
+  mongotools_url="https://fastdl.mongodb.org/tools/db/"${mongotools_tar_gz}
+
+
+  mongoshell_tar_gz="mongosh-1.8.2-linux-x64.tgz"
+  mongoshell_url="https://downloads.mongodb.com/compass/"${mongoshell_tar_gz}
 
   target_dir="/app/bin"
-  data_dir="/app/data"
-  log_dir="/app/log"
-  config_dir="/app/conf"
+  #data_dir="/app/data"
+  #log_dir="/app/log"
+  #config_dir="/app/conf"
 
   header "Installing MongoDB"
 
@@ -28,13 +35,19 @@ mongodb() {
   # Download and unpack mongodb
   pushd $target_dir
   run curl -O $mongo_url
+  run curl -O $mongotools_url
+  run curl -O $mongoshell_url
 
   mkdir mongodb
   pushd $target_dir/mongodb
   run tar --strip-components=1 -xzf ../$mongo_tar_gz
+  run tar --strip-components=1 -xzf ../$mongotools_tar_gz
+  run tar --strip-components=1 -xzf ../$mongoshell_tar_gz
   run chown -R mongodb:mongodb .
   popd
 
+  run rm $mongoshell_tar_gz
+  run rm $mongotools_tar_gz
   run rm $mongo_tar_gz
   popd
 
@@ -42,12 +55,12 @@ mongodb() {
   run mkdir -p $data_dir && chown -R mongodb:mongodb $data_dir
 
   # And the log file
-  run mkdir -p $log_dir && \
-    touch $log_dir/mongodb.log && chown mongodb:mongodb $log_dir/mongodb.log
+ # run mkdir -p $log_dir && \
+  #  touch $log_dir/mongodb.log && chown mongodb:mongodb $log_dir/mongodb.log
 
   # Copy our config file over.
-  run mkdir -p $config_dir && \
-    cp /pd_build/config/mongodb.conf $config_dir/
+  #run mkdir -p $config_dir && \
+   # cp /pd_build/config/mongodb.yml $config_dir/
 
   # Have mongodb start on container start
   run mkdir /etc/service/mongodb && \
